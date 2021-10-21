45b82d7a36a28a64633829a446d6b61ce79cce5b
MichelKhalaf <khalafmichel98@gmail.com>
2021-10-21 14:22:37 +0200

    Delete or move old files to archive

--- archive/api_radar.py
+++ archive/api_radar.py
@@ -1,22 +1,45 @@
-import time
-from flask import Flask, jsonify
-from ADSBC_live import ADSBClient, nb_vol, main
+# import time
+
+# from flask import Flask, jsonify, render_template
+# from ipyleaflet import Icon, Map, Marker, Polygon
+# from ipywidgets import HTML
+
+# from ADSBC_live import ADSBClient, main, nb_vol
+
+# app = Flask(__name__)
 
 
-app = Flask(__name__)
+# def calcul_list_vols() -> dict:
+#     return nb_vol(app.client)
 
 
-@app.route('/api/radarcape/')
-def vols():
-    xa = time.time()
-    print(app.client.queue.qsize())
-    resultats = nb_vol(app.client)
-    print(time.time()-xa)
-    return jsonify(resultats)
+# @app.route("/api/radarcape/", methods=["GET"])
+# def list_vols():
+#     xa = time.time()
+#     print(app.client.queue.qsize())
+#     resultats = calcul_list_vols()
+#     print(time.time() - xa)
+#     return jsonify(resultats)
 
 
-if __name__ == "__main__":
-    app.client = ADSBClient(host='134.212.189.239',
-                            port=10005, rawtype='beast')
-    main(app.client)
-    app.run(debug=True)
+# @app.route("/api/radarcape/map", methods=["GET"])
+# def create_map():
+#     resultats = calcul_list_vols()
+#     center = (43.57153, 1.4752)
+#     # icon=Icon(icon_url="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-plane-512.png")
+#     map = Map(center=center, zoom=5, close_popup_on_click=True)
+#     for m in resultats:
+#         marker = Marker(location=resultats[m][0]["ps"])
+#         map.add_layer(marker)
+#         message2 = HTML()
+#         message2.value = str(resultats[m][0]["ps"])
+#         marker.popup = message2
+#     map.save("radarcape/templates/my_map.html", title="My Map")
+#     return render_template("my_map.html")
+
+
+# if __name__ == "__main__":
+#     app.client = ADSBClient(host="134.212.189.239",
+#                             port=10005, rawtype="beast")
+#     main(app.client)
+#     app.run(debug=True)
