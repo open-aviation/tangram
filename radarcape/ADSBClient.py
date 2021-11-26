35e474f6e758ff58c5ffd5abaf9afc35d11f136d
Michel Khalaf <khalafmichel98@gmail.com>
2021-11-26 16:12:04 +0100

    Add option of importing data from file

---
+++ radarcape/ADSBClient.py
@@ -0,0 +1,156 @@
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+from datetime import timedelta, timezone
+
+import numpy as np
+import pandas as pd
+from traffic.core.traffic import Traffic
+from traffic.data import ModeS_Decoder
+
+
+def crit(df):
+    return (
+        df.vertical_rate_barometric_std - df.vertical_rate_inertial_std
+    ).abs()
+
+
+def thrushold(df):
+    return np.mean(df.criterion) + 1.2 * np.std(df.criterion)
+
+
+def turbulence(df):
+    return df.criterion > df.thrushold
+
+
+class ADSBClient:
+    def __init__(self, host: str, port: int, reference: str) -> None:
+        self.host = host
+        self.port = port
+        self.reference = reference
+        self.terminate = False
+        self.decoder = None
+        self._pro_data: Traffic = None  #: Dict[pd.Timestamp, Traffic] = {}
+        self._traffic: Traffic = None
+        self.lock_prodata = threading.Lock()
+        self.lock_traffic = threading.Lock()
+
+    def __init__(self) -> None:
+        self.terminate: bool = False
+        self.decoder: ModeS_Decoder = None
+        self._pro_data: Traffic = None  #: Dict[pd.Timestamp, Traffic] = {}
+        self._traffic: Traffic = None
+        self.lock_prodata = threading.Lock()
+        self.lock_traffic = threading.Lock()
+
+    @property
+    def pro_data(self):
+        # last entry
+        with self.lock_prodata:
+            return self._pro_data
+
+    @property
+    def traffic(self):
+        # last entry
+        with self.lock_traffic:
+            return self._traffic
+
+    def calculate_traffic(self):
+        with self.lock_traffic:
+            try:
+                self._traffic = (
+                    self.decoder.traffic.longer_than("1T")
+                    .last("30T")
+                    .resample("1s")
+                    .eval(max_workers=4)
+                )
+            except Exception as e:
+                print(e)
+
+    def turbulence(self):
+        self.calculate_traffic()
+        if self._traffic is not None:
+            with self.lock_prodata:
+                try:
+                    self._pro_data = (
+                        self._traffic
+                        .filter(  # median filters for abnormal points
+                            vertical_rate_barometric=3,
+                            vertical_rate_inertial=3,  # kernel sizes
+                            strategy=None,  # invalid data becomes NaN
+                        )
+                        .agg_time(
+                            # aggregate data over intervals of one minute
+                            "1 min",
+                            # compute the std of the data
+                            vertical_rate_inertial="std",
+                            vertical_rate_barometric="std",
+                            # reduce one minute to one point
+                            latitude="mean",
+                            longitude="mean",
+                        )
+                        .assign(
+                            # we define a criterion based on the
+                            # difference between two standard deviations
+                            # on windows of one minute
+                            criterion=crit
+                        )
+                        .assign(
+                            # we define a thushold based on the
+                            # mean criterion + 1.2 * standar deviation criterion
+                            thrushold=thrushold
+                        )
+                        .assign(turbulence=turbulence)
+                        .eval(max_workers=4)
+                    )
+                except Exception as e:
+                    print(e)
+
+    def calculate_live_turbulence(self):
+        while not self.terminate:
+            self.turbulence()
+            time.sleep(1)
+
+    def start_live(self, host: str, port: int, reference: str):
+        self.decoder = ModeS_Decoder.from_address(
+            host, port, reference
+        )
+        executor = ThreadPoolExecutor(max_workers=4)
+        executor.submit(self.calculate_live_turbulence)
+        executor.submit(self.clean_decoder)
+
+    def start_from_file(self, file: str, reference: str):
+        self.decoder = ModeS_Decoder.from_file(
+            file, template="time,df,icao,shortmsg", reference=reference)
+        self.turbulence()
+
+    def __exit__(self):
+        self.stop()
+
+    def stop(self):
+        self.decoder.stop()
+        self.terminate = True
+
+    def clean_decoder(self):
+        while not self.terminate:
+            for icao in list(self.decoder.acs.keys()):
+                condition = True
+                with self.decoder.acs[icao].lock:
+                    if len(self.decoder.acs[icao].cumul) > 0:
+                        condition = False
+                        if pd.Timestamp(
+                            "now", tzinfo=timezone.utc
+                        ) - self.decoder.acs[icao].cumul[-1][
+                            "timestamp"
+                        ] >= timedelta(
+                            hours=1
+                        ):
+                            del self.decoder.acs[icao]
+                if condition and self.decoder.acs[icao].flight is not None:
+                    if pd.Timestamp(
+                        "now", tzinfo=timezone.utc
+                    ) - self.decoder.acs[icao].flight.stop >= timedelta(
+                        hours=1
+                    ):
+                        with self.decoder.acs[icao].lock:
+                            del self.decoder.acs[icao]
--- radarcape/__init__.py
+++ radarcape/__init__.py
@@ -2,29 +2,35 @@ from atmlab.weather import Weather
 from flask import Flask
 from flask_assets import Environment
 
-from .ADSBC_live import ADSBClient
+from .ADSBClient import ADSBClient
 from .util import assets
 
 import configparser
 from pathlib import Path
 
-config_dir = Path("/home/mkhalaf/reproduce")
-config_file = config_dir / "mongo.conf"
+# import logging
+# log = logging.getLogger('werkzeug')
+# log.setLevel(logging.ERROR)
+
+config_dir = Path("/home/mkhalaf/api_radarcape")
+config_file = config_dir / "config.conf"
 config = configparser.ConfigParser()
 config.read(config_file.as_posix())
 mongo_username = config.get("mongo", "username", fallback="")
 mongo_password = config.get("mongo", "password", fallback="")
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.client = ADSBClient(
-        host="134.212.189.239", port=10005, reference="LFBO"
-    )
-    app.client.start()
+    app.client = ADSBClient()
+    app.client.start_live(host="134.212.189.239", port=10005, reference="LFBO")
 
-    app.sigmet = Weather(username="iesta", password="!dtis2050")
+    app.sigmet = Weather(username=mongo_username, password=mongo_password)
     app.sigmet.session.proxies.update(
-        {"http": "http://proxy.onera:80", "https": "https://proxy.onera:80"}
+        {
+            "http": config.get("proxy", "proxy_http", fallback=""),
+            "https": config.get("proxy", "proxy_https", fallback="")
+        }
     )
     from . import views
 
