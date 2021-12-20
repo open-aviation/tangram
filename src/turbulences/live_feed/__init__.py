98b9d992aa78e48f3ea154a3cdbd16b6512a4dfb
Michel Khalaf <khalafmichel98@gmail.com>
2021-12-20 15:03:59 +0100

    Merge the 2 applications

--- src/turbulences/live_feed/__init__.py
+++
@@ -1,9 +0,0 @@
-import configparser
-from pathlib import Path
-
-from appdirs import user_config_dir
-
-config_dir = Path(user_config_dir("atmlab"))
-config_file = config_dir / "atmlab.conf"
-config = configparser.ConfigParser()
-config.read(config_file.as_posix())
