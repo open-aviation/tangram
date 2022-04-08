6329617830e68fe4a613744b339421e0430981fc
Michel Khalaf <khalafmichel98@gmail.com>
2022-04-08 16:52:53 +0200

    test fixes url

--- src/turbulences/__main__.py
+++ src/turbulences/__main__.py
@@ -12,9 +12,9 @@ from flask_pymongo import PyMongo
 
 from turbulences import config
 
-from client.ADSBClient import ADSBClient
-from util import assets
-from views import base_views, history_views
+from .client.ADSBClient import ADSBClient
+from .util import assets
+from .views import base_views, history_views
 
 from waitress import serve
 
@@ -45,6 +45,19 @@ def list_routes():
     # app.url_map.
     a = url_for("base.home_page")
     return {"route": ["%s" % rule for rule in app.url_map.iter_rules()]}
+
+
+def serve_app(app):
+    class ScriptNameStripper(object):
+        def __init__(self, app):
+            self.app = app
+
+        def __call__(self, environ, start_response):
+            environ["SCRIPT_NAME"] = "turbulence/stable/"
+            return self.app(environ, start_response)
+
+    app = ScriptNameStripper(app)
+    serve(app, host="0.0.0.0", port=6001, threads=8)
 
 
 def main():
@@ -82,20 +95,10 @@ def main():
     app.network = Network()
     app_host = config.get("application", "host", fallback="0.0.0.0")
     app_port = int(config.get("application", "port", fallback=5000))
+    serve_app(app)
 
     # app.run(host=app_host, port=app_port)
 
 
 if __name__ == "__main__":
     main()
-
-    class ScriptNameStripper(object):
-        def __init__(self, app):
-            self.app = app
-
-        def __call__(self, environ, start_response):
-            environ["SCRIPT_NAME"] = "turbulence/stable/"
-            return self.app(environ, start_response)
-
-    app = ScriptNameStripper(app)
-    serve(app, host="0.0.0.0", port=6001, threads=8)
