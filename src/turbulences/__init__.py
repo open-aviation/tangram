7c94792d593e8fcc9e13940d506e9530d7ce20db
Michel Khalaf <khalafmichel98@gmail.com>
2022-05-04 16:47:16 +0200

    Create a conf file for decoder_app

--- src/turbulences/__init__.py
+++ src/turbulences/__init__.py
@@ -7,3 +7,7 @@ config_dir = Path(user_config_dir("atmlab"))
 config_file = config_dir / "atmlab.conf"
 config_turb = configparser.ConfigParser()
 config_turb.read(config_file.as_posix())
+
+config_file = config_dir / "decoder.conf"
+config_decoder = configparser.ConfigParser()
+config_decoder.read(config_file.as_posix())
