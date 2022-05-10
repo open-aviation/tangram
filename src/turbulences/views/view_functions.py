bc26c32ca991382cb593328d326cebc31a4d60c6
Michel Khalaf <khalafmichel98@gmail.com>
2022-05-10 23:47:53 +0200

    parallelisation of planes request

--- src/turbulences/views/view_functions.py
+++ src/turbulences/views/view_functions.py
@@ -1,34 +1,41 @@
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
-from traffic.core.traffic import Traffic
+from traffic.core import Traffic, Flight
 
 
-def geojson_plane(data: Traffic) -> dict:
+def geojson_flight(flight: Flight) -> dict:
+    data = flight.data
+    latitude = data.latitude.iloc[-1]
+    longitude = data.longitude.iloc[-1]
+    track = data.track.iloc[-1]
+    if not (np.isnan(latitude) and np.isnan(longitude)):
+        x = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    longitude,
+                    latitude,
+                ],
+            },
+            "properties": {
+                "icao": flight.icao24,
+                "callsign": flight.callsign,
+                "typecode": flight.typecode,
+                "dir": 0 if np.isnan(track) else track,
+            },
+        }
+        return x
+    return None
+
+
+def geojson_traffic(traffic: Traffic) -> dict:
     features = []
-    if data is not None:
-        for flight in data:
-            # if flight.shape is not None:
-            data = flight.data
-            latitude = data.latitude.iloc[-1]
-            longitude = data.longitude.iloc[-1]
-            track = data.track.iloc[-1]
-            if not (np.isnan(latitude) and np.isnan(longitude)):
-                x = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [
-                            longitude,
-                            latitude,
-                        ],
-                    },
-                    "properties": {
-                        "icao": flight.icao24,
-                        "callsign": flight.callsign,
-                        "typecode": flight.typecode,
-                        "dir": 0 if np.isnan(track) else track,
-                    },
-                }
-                features.append(x)
+    if traffic is not None:
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            features = list(
+                executor.map(geojson_flight, traffic)
+            )
     geojson = {
         "type": "FeatureCollection",
         "features": features,
