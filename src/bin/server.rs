7a7c2b4097cdb31147ba623799c4625c386dd19c
xiaogang <maple.hl@gmail.com>
2024-06-16 21:52:05 +0800

    cleanup structures

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::fmt::{Display, Error};
 use std::net::SocketAddr;
 use std::path::PathBuf;
@@ -10,12 +12,13 @@ use axum::{Extension, Router};
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde::{Deserialize, Serialize};
 use serde_tuple::{Deserialize_tuple, Serialize_tuple};
+use tokio::net::UdpSocket;
 use tokio::sync::Mutex;
+use tokio_tungstenite::connect_async;
 use tower_http::services::ServeDir;
 use tracing::{debug, error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use uuid::Uuid;
-
 use websocket_channels::ChannelManager;
 
 // use tracing::log::info;
@@ -35,6 +38,99 @@ use websocket_channels::ChannelManager;
 // < [join_ref, ref, topic, 'phx_reply', payload]
 //
 
+#[derive(Debug, Serialize, Deserialize)]
+struct EmptyResponse {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct HeartbeatResponse {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct JoinResponse {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LeaveResponse {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DatetimeResponse {
+    datetime: String,
+    counter: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum Response {
+    EmptyResponse {},
+    HeartbeatResponse {},
+    JoinResponse {},
+    LeaveResponse {},
+    DatetimeReesponse { datetime: String, counter: u32 },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ReplyPayload {
+    status: String,
+    response: Response,
+}
+
+#[derive(Debug, Serialize_tuple, Deserialize_tuple)]
+struct ReplyTuple {
+    join_reference: Option<String>, // null when it's heartbeat
+    reference: String,
+    topic: String, // `channel`
+    event: String,
+    payload: ReplyPayload, // TODO
+}
+
+impl Display for ReplyTuple {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
+        write!(
+            formatter,
+            "<ReplyTuple: join_ref={:?}, ref={}, topic={}, event={}>",
+            self.join_reference, self.reference, self.topic, self.event
+        )
+    }
+}
+
+/// request data structures
+
+#[derive(Debug, Serialize, Deserialize)]
+struct JoinRequest {
+    token: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LeaveRequest {}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct HeartbeatRequest {}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum RequestPayload {
+    JoinRequest { token: String },
+    LeaveRequest {},
+    HeartbeatRequest {},
+}
+
+#[derive(Debug, Serialize_tuple, Deserialize_tuple)]
+struct RequestTuple {
+    join_reference: Option<String>, // null when it's heartbeat
+    reference: String,
+    topic: String, // `channel`
+    event: String,
+    payload: RequestPayload, // TODO
+}
+
+impl Display for RequestTuple {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
+        write!(
+            formatter,
+            "<RequestTuple: join_ref={:?}, ref={}, topic={}, event={}>",
+            self.join_reference, self.reference, self.topic, self.event
+        )
+    }
+}
+
 struct User {
     user_id: String,
     session_id: i32,
@@ -48,6 +144,28 @@ struct State {
     channels: Mutex<ChannelManager<String>>, // String: message type, TODO customize this
 }
 
+/// a websocket client, subscribe to binary data
+async fn websocket_client(local_state: Arc<State>, channel_name: &str) {
+    loop {
+        let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
+        socket.connect("127.0.0.1:42125").await.unwrap();
+
+        // Connect to the websocket endpoint
+        let url = "ws://51.158.72.24:1234/42125@LFBO";
+        let (ws_stream, _) = connect_async(url)
+            .await
+            .expect("fail to connect to websocket endpoint");
+
+        let (_, ws_rx) = ws_stream.split();
+        ws_rx
+            .for_each(|message| async {
+                let raw_data = message.unwrap().into_data();
+                info!("raw message size: {:?}", raw_data.len());
+            })
+            .await;
+    }
+}
+
 async fn timestamp_task(local_state: Arc<State>, channel_name: &str) {
     tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
 
@@ -56,12 +174,12 @@ async fn timestamp_task(local_state: Arc<State>, channel_name: &str) {
     let event = "datetime";
     loop {
         let now = chrono::Local::now();
-        let message = ChannelMessage {
+        let message = ReplyTuple {
             join_reference: None,
             reference: counter.to_string(),
             topic: channel_name.to_string(),
             event: event.to_string(),
-            payload: Payload::Reply {
+            payload: ReplyPayload {
                 status: "ok".to_string(),
                 response: Response::DatetimeReesponse {
                     datetime: now.to_rfc3339_opts(chrono::SecondsFormat::Millis, false),
@@ -113,8 +231,8 @@ async fn main() {
         .route("/websocket", get(websocket_handler))
         .layer(Extension(state.clone()));
 
-    let local_state = state.clone();
-    tokio::spawn(timestamp_task(local_state, "system"));
+    tokio::spawn(timestamp_task(state.clone(), "system"));
+    tokio::spawn(websocket_client(state.clone(), "system"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
     axum::serve(
@@ -134,88 +252,20 @@ async fn websocket_handler(
     ws.on_upgrade(|socket| websocket(socket, state))
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct JoinPayload {
-    token: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct JoinReply {
-    status: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct LeaveRequest {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct HeartbeatRequest {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct HeartbeatReply {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct EmptyResponse {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct DatetimeResponse {
-    datetime: String,
-    counter: u32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum Response {
-    DatetimeReesponse { datetime: String, counter: u32 },
-    EmptyResponse {},
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Reply {
-    status: String,
-    response: Response,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum Payload {
-    JoinRequest { token: String },
-    JoinReply { status: String },
-    LeaveRequest {},
-    Reply { status: String, response: Response },
-}
-
-#[derive(Debug, Serialize_tuple, Deserialize_tuple)]
-struct ChannelMessage {
-    join_reference: Option<String>, // null when it's heartbeat
-    reference: String,
-    topic: String, // `channel`
-    event: String,
-    payload: Payload, // TODO
-}
-
-impl Display for ChannelMessage {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> Result<(), Error> {
-        write!(
-            formatter,
-            "<ChannelMessage: join_ref={:?}, ref={}, topic={}, event={}>",
-            self.join_reference, self.reference, self.topic, self.event
-        )
-    }
-}
-
 async fn send_ok(
     join_reference: Option<String>,
     reference: &str,
     channel: &str,
     state: Arc<State>,
 ) {
-    let join_reply = ChannelMessage {
+    let join_reply = ReplyTuple {
         join_reference: join_reference.clone(),
         reference: reference.to_string(),
         topic: channel.to_string(),
         event: "phx_reply".to_string(),
-        payload: Payload::JoinReply {
+        payload: ReplyPayload {
             status: "ok".into(),
+            response: Response::EmptyResponse {},
         },
     };
     let text = serde_json::to_string(&join_reply).unwrap();
@@ -232,7 +282,7 @@ async fn send_ok(
 async fn handle_incoming_messages(received_text: String, state: Arc<State>, user_id: &str) {
     debug!("< {}", received_text);
 
-    let received_message: ChannelMessage = serde_json::from_str(&received_text).unwrap();
+    let received_message: RequestTuple = serde_json::from_str(&received_text).unwrap();
     debug!("{}", received_message);
 
     let reference = &received_message.reference;
