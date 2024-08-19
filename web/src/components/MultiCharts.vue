78616d72196cd808d318535fd80c9d2dc11b9523
liuyuntian <liuyuntian@shinetechsoftware.com>
2024-08-19 22:24:45 +0800

    feat ui change

--- web/src/components/MultiCharts.vue
+++ web/src/components/MultiCharts.vue
@@ -25,6 +25,47 @@ import { Line as LineChart } from 'vue-chartjs'
 import dayjs from 'dayjs'
 import { useMapStore} from '../store'
 
+const plugin = {
+  id: 'corsair',
+  defaults: {
+    width: 1,
+    color: '#FF4949',
+    dash: [3, 3],
+  },
+  afterInit: (chart, args, opts) => {
+    chart.corsair = {
+      x: 0,
+      y: 0,
+    }
+  },
+  afterEvent: (chart, args) => {
+    const {inChartArea} = args
+    const {type,x,y} = args.event
+
+    chart.corsair = {x, y, draw: inChartArea}
+    chart.draw()
+  },
+  beforeDatasetsDraw: (chart, args, opts) => {
+    const {ctx} = chart
+    const {top, bottom, left, right} = chart.chartArea
+    const {x, y, draw} = chart.corsair
+    if (!draw) return
+
+    ctx.save()
+
+    ctx.beginPath()
+    ctx.lineWidth = opts.width
+    ctx.strokeStyle = opts.color
+    ctx.setLineDash(opts.dash)
+    ctx.moveTo(x, bottom)
+    ctx.lineTo(x, top)
+    ctx.moveTo(left, y)
+    ctx.lineTo(right, y)
+    ctx.stroke()
+
+    ctx.restore()
+  }
+}
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -33,7 +74,8 @@ ChartJS.register(
     Title,
     Tooltip,
     Legend,
-    Filler
+    Filler,
+    plugin
 )
 
 export default {
@@ -253,6 +295,7 @@ export default {
                 }
               ]
             }
+
             this.option = {
               onHover: (e) => {
                 const chart = this.$refs.chart.chart;
@@ -296,6 +339,9 @@ export default {
                 },
                 filler: {
                   propagate: true
+                },
+                corsair: {
+                  color: 'black',
                 }
               }
             }
