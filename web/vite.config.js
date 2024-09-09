bdc92d2b9b5121778b281889494e8729f3a83e19
xiaogang <maple.hl@gmail.com>
2024-09-09 21:48:06 +0800

    use TANGRAM_HOST from env, or default to 127.0.0.1

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,26 +2,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/dist/vite'
 
-let host = '127.0.0.1';
+let tangram_host = process.env.TANGRAM_HOST || '127.0.0.1';
 
 export default defineConfig({
   server: {
     proxy: {
       // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
       // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      '/data': `http://${host}:18000`,
+      '/data': `http://${tangram_host}:18000`,
       '^/plugins.*': {
-        target: `https://${host}:18000/plugins.*`,
+        target: `https://${tangram_host}:18000/plugins.*`,
         changeOrigin: true,
       },
       '^/flight/.*': {
-        target: `https://${host}:18000/flight/.*`,
+        target: `https://${tangram_host}:18000/flight/.*`,
         changeOrigin: true,
       },
       // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
       // Exercise caution using `rewriteWsOrigin` as it can leave the proxying open to CSRF attacks.
       '/websocket': {
-        target: `ws://${host}:18000/websocket?userToken=joining-token&vsn=2.0.0`,
+        target: `ws://${tangram_host}:18000/websocket?userToken=joining-token&vsn=2.0.0`,
         ws: true,
         rewriteWsOrigin: true,
       },
