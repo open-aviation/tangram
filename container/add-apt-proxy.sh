fb5d7a8f669cbb61c0eab415556c32f9481a5bb2
xiaogang <maple.hl@gmail.com>
2025-01-07 20:22:30 +0800

    remove hard coded proxy

--- container/add-apt-proxy.sh
+++ container/add-apt-proxy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-set -x -euo pipefail
+set -x -eo pipefail
 
 # Get HTTP proxy, fallback to HTTPS proxy if not set
-# http_proxy_value=${http_proxy:-$https_proxy}
-http_proxy_value="http://192.168.11.1:1081"
+http_proxy_value=${http_proxy:-$https_proxy}
 
 # Get HTTPS proxy, fallback to HTTP proxy if not set
-# https_proxy_value=${https_proxy:-$http_proxy}
-https_proxy_value="http://192.168.11.1:1081"
+https_proxy_value=${https_proxy:-$http_proxy}
 
 # If either proxy is set, add to apt.conf
 if [ -n "$http_proxy_value" ] || [ -n "$https_proxy_value" ]; then
